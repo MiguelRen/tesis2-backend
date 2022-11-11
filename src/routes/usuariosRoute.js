ab164ee4c64d06d0c756c5e3334e704978cf7c02
MiguelRen <71660206+MiguelRen@users.noreply.github.com>
2022-11-10 22:38:09 -0400

    Delete src directory

--- src/routes/usuariosRoute.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { methods as usuariosController } from  '../controllers/usuariosController'
-
-const router = Router();
-router.get("/api/Usuarios" , usuariosController.getUsuarios );
-
-export default router;
